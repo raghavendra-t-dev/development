5cd60c66ef96853d80cd9d39bb1a24ae6284c2a3
Tirumalasetti Raghavendra <t.raghavendra@globallogic.com>
2025-11-03 17:13:52 +0530

    Button Component

---
+++ src/Components/Button.js
@@ -0,0 +1,10 @@
+const Button =()=>{
+    return(
+        <div>
+            <p>Button Component</p>
+            <button>Complete Deployment</button>
+        </div>
+    )
+}
+
+export default Button
