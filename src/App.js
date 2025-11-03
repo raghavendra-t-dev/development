4794ac00f54c57bb3ba4b12f0149e6720cfffa65
Tirumalasetti Raghavendra <t.raghavendra@globallogic.com>
2025-11-03 17:38:47 +0530

    Counter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button'
+import Counter from './Components/Counter';
 
 function App() {
   return (
@@ -18,7 +19,8 @@ function App() {
         >
           Learn React
         </a>
-        <Button/ >
+        <Button/>
+        <Counter/>
       </header>
     </div>
   );
---
+++ src/Components/Counter.js
@@ -0,0 +1,9 @@
+const Counter=()=>{
+    return(
+        <div>
+            <p>Counter Component</p>
+        </div>
+    )
+}
+
+export default Counter
